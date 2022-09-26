94d1109c2408b1d030bc4196d64defec9a101eba
felipeazs <felipeazs@gmail.com>
2022-09-26 19:11:07 -0300

    deploy v4

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+require('dotenv').config();
+
 const app = express();
 
 app.get('/', (req, res, next) => {
 	console.log('Hello World!');
 	res.send('Hello World!');
 });
+
+//Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    //create static build folder for the frontend
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    //serve index.html for all routes
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).send('API is running...')
+    })
+}
 
 app.listen(5001, () => {
 	console.log('Listening on port 5001');
