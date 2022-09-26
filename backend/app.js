be795226686577415a06bf193212e214e2527d59
felipeazs <felipeazs@gmail.com>
2022-09-26 19:15:33 -0300

    deploy v5

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -10,15 +11,15 @@ app.get('/', (req, res, next) => {
 
 //Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-    //create static build folder for the frontend
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+	//create static build folder for the frontend
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-    //serve index.html for all routes
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+	//serve index.html for all routes
+	app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
 } else {
-    app.get('/', (req, res) => {
-        res.status(200).send('API is running...')
-    })
+	app.get('/', (req, res) => {
+		res.status(200).send('API is running...');
+	});
 }
 
 app.listen(5001, () => {
